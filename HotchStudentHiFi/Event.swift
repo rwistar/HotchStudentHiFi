fcfb1b6377d209a3913c54acf274356d4cb16bcb
Roger Wistar <rwistar@hotchkiss.org>
2020-06-01 16:43:01 -0400

    Working on events view

---
+++ HotchStudentHiFi/Event.swift
@@ -0,0 +1,16 @@
+//
+//  Event.swift
+//  HotchStudentHiFi
+//
+//  Created by Roger P Wistar on 6/1/20.
+//  Copyright © 2020 Roger P Wistar. All rights reserved.
+//
+
+import Foundation
+
+struct Event {
+    var title: String
+    var desc: String
+    var date: Date
+    var loc: String
+}
